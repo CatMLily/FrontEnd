6b2ee958cc5a6c9fbbf23af9b479069ffff435d5
CatM <240461738@qq.com>
2017-08-20 22:53:48 +0800

    read imageInfo from json

--- Gallery/js/index.js
+++ Gallery/js/index.js
@@ -1,9 +1,34 @@
-
 window.onload = function () {
+  readJSON();
   // 绑定关联的click
   preGallery();
 };
 let imgList = document.getElementById('imagegallary');
+function readJSON() {
+  const url = "../Gallery/image.json";
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState === 4) {
+      if(xhr.status === 200) {
+        const myImgList = JSON.parse(xhr.responseText);
+        console.log(myImgList);
+        myImgList.map(function (e) {
+          let elePNode = document.createElement('li');
+          let eleCNode = document.createElement('a');
+          eleCNode.setAttribute('href',e.imgSrc);
+          eleCNode.setAttribute('title',e.imgTitle);
+          eleCNode.innerHTML = e.description;
+          elePNode.appendChild(eleCNode);
+          imgList.appendChild(elePNode);
+        });
+      } else {
+        console.log(xhr.status);
+      }
+    }
+  };
+  xhr.open("GET",url,true);
+  xhr.send(null);
+}
 function preGallery() {
   if(!imgList) return false;
   imgList.addEventListener('click',function (e) {
@@ -30,3 +55,5 @@ function showPic(picNum) {
   }
   return true;
 }
+
+
